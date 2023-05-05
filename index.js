a05767f20c26e93d5f61d411518f13a14127c465
helenamaltseva <helena.maltseva@gmail.com>
2023-05-05 23:42:15 +0300

    Created new files

---
+++ index.js
@@ -0,0 +1,13 @@
+// let sum = 0.1 + 0.2;
+// console.log(sum.toFixed(2));
+
+let str = "1";
+let num = 2;
+let sum = parseInt(str) + num;
+console.log(sum);
+
+let flashSize = prompt("16");
+let fileSize = 820; 
+let numberOfFiles = Math.floor(flashSize * 1024 / fileSize);
+console.log(`${numberOfFiles}`);
+
